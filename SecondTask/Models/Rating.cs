c35778a79db8a0c98a0ff7aef5fdebcd0bfae9e3
SHDarking621 <dmytro.hladkyi@nure.ua>
2022-08-11 13:19:29 +0300

    feat: 1. Create models fo db 2. Clear template

---
+++ SecondTask/Models/Rating.cs
@@ -0,0 +1,9 @@
+﻿namespace SecondTask.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public decimal Score { get; set; }
+    }
+}
---
+++ SecondTask/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace SecondTask.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public int BookId { get; set; }
+        public string Reviewer { get; set; }
+
+    }
+}
